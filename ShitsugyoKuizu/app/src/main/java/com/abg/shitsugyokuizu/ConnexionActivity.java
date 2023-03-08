849964b9c008361e6e8ab56603ca17cdd50ef2b9
TsukuyomiKun3 <onur.genc03@mail.com>
2023-03-08 17:08:00 +0100

    Design de la page de ConnexionActivity

--- ShitsugyoKuizu/app/src/main/java/com/abg/shitsugyokuizu/ConnexionActivity.java
+++ ShitsugyoKuizu/app/src/main/java/com/abg/shitsugyokuizu/ConnexionActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Connexion_Activity extends AppCompatActivity {
+public class ConnexionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
