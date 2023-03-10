1528ad8347478cabb4929eccd7bb52c412493e13
Lucas Astier <parukia77@gmail.com>
2023-03-10 14:38:25 +0100

    ajout de la fonction pour se connecter

--- ShitsugyoKuizu/app/src/main/java/com/abg/shitsugyokuizu/User.php
+++ ShitsugyoKuizu/app/src/main/java/com/abg/shitsugyokuizu/User.php
@@ -4,11 +4,18 @@
 
         public function __construct(){}
 
-        public function getUserData($username, $password){
+        public function connect($username, $password){
             $query = self::$bdd->prepare("SELECT * FROM users WHERE username = :username");
             $query->execute(array(":username" => $username));
             $row = $query->fetch();
-            return $row;
+            if($row){
+                if(password_verify($password, $row['password'])){
+                    $_SESSION['id'] = $row['id'];
+                    header('Location: index.php');
+                }else{
+                    echo "Incorrect information";
+                }
+            }
         }
 
     }
