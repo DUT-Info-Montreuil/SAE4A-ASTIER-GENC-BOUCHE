54de73c2edbc2d9d83945bc0fca3980b0dbe9a74
Lucas Astier <parukia77@gmail.com>
2023-03-10 14:45:09 +0100

    correction de la page User

--- ShitsugyoKuizu/app/src/main/java/com/abg/shitsugyokuizu/User.php
+++ ShitsugyoKuizu/app/src/main/java/com/abg/shitsugyokuizu/User.php
@@ -5,23 +5,22 @@
         public function __construct(){}
 
         public function connect($username, $password){
-            $query = self::$bdd->prepare("SELECT * FROM users WHERE username = :username");
+            $query = self::$bdd->prepare("SELECT * FROM utilisateur WHERE pseudo = :username");
             $query->execute(array(":username" => $username));
             $row = $query->fetch();
             if($row){
-                if(password_verify($password, $row['password'])){
+                if(password_verify($password, $row['motdepasse'])){
                     $_SESSION['id'] = $row['id'];
-                    header('Location: index.php');
                 }else{
                     echo "Incorrect information";
                 }
             }
         }
 
-        public function signUp($username, $unHashedPassword, $email, $promotion){
+        public function signUp($username, $unHashedPassword, $email, ){
             $hash = password_hash($unHashedPassword, PASSWORD_DEFAULT);
-            $query = self::$bdd->prepare("INSERT INTO users (username, password, email, promotion) VALUES (:username, :password, :email, :promotion)");
-            $query->execute(array(":username" => $username, ":password" => $hash, ":email" => $email, ":promotion" => $promotion));
+            $query = self::$bdd->prepare("INSERT INTO utilisateur (pseudo, motdepasse, email) VALUES (:username, :password, :email, )");
+            $query->execute(array(":username" => $username, ":password" => $hash, ":email" => $email));
         }
 
     }
