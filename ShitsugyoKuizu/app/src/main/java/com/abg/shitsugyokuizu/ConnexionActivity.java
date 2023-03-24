565f675f415bccdb1facce4a6885990dd7042070
TheblackReaper060303 <96045738+TheblackReaper060303@users.noreply.github.com>
2023-03-24 17:47:05 +0100

    correction de bug et implémentation d'api

--- ShitsugyoKuizu/app/src/main/java/com/abg/shitsugyokuizu/ConnexionActivity.java
+++ ShitsugyoKuizu/app/src/main/java/com/abg/shitsugyokuizu/ConnexionActivity.java
@@ -3,12 +3,78 @@ package com.abg.shitsugyokuizu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ConnexionActivity extends AppCompatActivity {
+    private EditText pseudo;
+    private EditText mdp;
+    private Button connexion;
+    private Button inscription;
+
+    public final String urlApi ="http://10.0.2.2/connexion_app.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
+        pseudo = findViewById(R.id.login_Edit_Text);
+        mdp = findViewById(R.id.mdp_Edit_Text);
+        connexion = findViewById(R.id.Connexion_Button);
+        inscription = findViewById(R.id.Inscription_Button);
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+
+        connexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                StringRequest request = new StringRequest(Request.Method.POST, urlApi, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(ConnexionActivity.this, response, Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        
+                    }
+
+                }){
+                    @Override
+                    protected Map<String, String> getParams(){
+                        Map<String, String> parametres = new HashMap<>();
+                        parametres.put("pseudo", pseudo.getText().toString());
+                        parametres.put("mdp", mdp.getText().toString());
+                        return parametres;
+                    }
+                };
+                
+                queue.add(request);
+
+            }
+
+        });
+
+        inscription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+
     }
 }
